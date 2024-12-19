060c9ba4e151bf702fdc3e02b691b33172b32642
Jonathan Lee <jonathan.lee.devel@gmail.com>
2024-12-19 01:32:01 +0000

    Add Astro types and lock dependencies
    Added a new TypeScript type reference for the Astro client and generated a fresh `yarn.lock` file, locking updated or new dependencies for the project. These changes ensure proper dependency resolution and type safety in the development environment.

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,5 +9,9 @@ import vercel from "@astrojs/vercel/serverless";
 export default defineConfig({
   integrations: [tailwind(), icon(), compress()],
   output: "server",
-  adapter: vercel()
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    }
+  })
 });
